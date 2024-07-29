fae5e51848826347583282f2e1a5ac02462074b1
dawoodtrumboo <thedmax0007@gmail.com>
2024-07-29 16:48:41 +0530

    Google Auth

--- app/auth/google/callback/page.tsx
+++ app/auth/google/callback/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useContext, useEffect, useLayoutEffect, useRef } from 'react'
+import React, { Suspense, useContext, useEffect, useLayoutEffect, useRef } from 'react'
 import GoogleCallback from '../../components/GoogleCallback'
 import { useSearchParams,useRouter } from 'next/navigation'
 import { googleSignin } from '@/services/authApi'
@@ -10,40 +10,13 @@ import { StoreContext } from '@/app/context/context'
 
 const Page = () => {
 
-  const{setAuth} = useContext(StoreContext)
-  const params = useSearchParams()
-  const code = params.get('code')
-  const router = useRouter()
-  const callRef = useRef(null)
-  console.log(code)
-  useLayoutEffect(() => {
-    
-    const fetchUserData = async () => {
-     
-      try {
-        const data = await googleSignin(code);
-        console.log(data)
-        if (data) {
-          localStorage.setItem("userDetails", JSON.stringify(data));
-          setAuth(data);
-          router.replace("/tasks");
-      
-        }
-      } catch (error) {
-        console.error("Failed to fetch user data:", error);
-      }
-    };
 
-    if (code && !callRef.current) {
-      fetchUserData();
-      callRef.current = true;
-    }
-  }, []);
 
   return (
-    <div>
+    <Suspense>
     <GoogleCallback/>
-    </div>
+    </Suspense>
+    
   )
 }
 
